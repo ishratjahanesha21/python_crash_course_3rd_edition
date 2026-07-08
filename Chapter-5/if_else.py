38ba10cf94e8edc335720111993a6ad524492ad7
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2026-07-07 23:25:20 -0700

    add if-else

---
+++ Chapter-5/if_else.py
@@ -0,0 +1,13 @@
+cars=["bmw", "mercedes", "audi", "porsche"]
+# make all car name in title case but in case of bmw we want to make it in uppercase.
+for car in cars:
+    if car=="bmw":
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+# ALL the if statment has two result that is boolen . bollen have two value True or False. if the condition is true then it will execute the code inside the if block and if the condition is false then it will execute the code inside the else block. 
+# so true and false are conditional test 
+#   oparetion parameters ARE  used to compare the values and return a boolean value. there are six conditional test operators that are used to compare the values.
+# 1. Equal to (==) 2. Not equal to (!=) 3. Greater than (>) 4. Less than (<) 5. Greater than or equal to (>=) 6. Less than or equal to (<=) , and the logical operators (and, or, not) to combine these conditions. add && or || and not are used to combine the conditions.
