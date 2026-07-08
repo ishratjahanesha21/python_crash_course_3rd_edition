b3961e6ff4cd3faf7da0879b8a0d01478a758021
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2026-07-07 23:59:50 -0700

    add

--- Chapter-5/if_else.py
+++ Chapter-5/if_else.py
@@ -11,3 +11,37 @@ for car in cars:
 # so true and false are conditional test 
 #   oparetion parameters ARE  used to compare the values and return a boolean value. there are six conditional test operators that are used to compare the values.
 # 1. Equal to (==) 2. Not equal to (!=) 3. Greater than (>) 4. Less than (<) 5. Greater than or equal to (>=) 6. Less than or equal to (<=) , and the logical operators (and, or, not) to combine these conditions. add && or || and not are used to combine the conditions.
+magicians = ["alisha", "ben", "charlie", "diana"]
+for magician in magicians:
+    print(magician)
+    
+# 1. Even or Odd ⭐ (Easy)
+giving_number = int(input("Enter a number: "))
+if giving_number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Age Category ⭐ (Easy)    # 
+giving_age = int(input("Enter your age: "))
+if (giving_age < 13):
+    print("You are a child.")
+
+elif (giving_age >= 13 and giving_age <= 19):
+    print("You are a teenager.")
+
+elif(giving_age >= 20 and giving_age <= 59):
+    print("You are an adult.")
+elif(giving_age >= 60):
+    print("You are a senior citizen.")   
+
+
+    # 
+#  check the positive and nagative number 
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
--- chapter-4/WorkingWithLoop.py
+++ chapter-4/WorkingWithLoop.py
@@ -2,3 +2,33 @@ magicians = ["alisha", "ben", "charlie", "diana"]
 for magician in magicians:
     print(magician)
     
+# 1. Even or Odd ⭐ (Easy)
+giving_number = int(input("Enter a number: "))
+if giving_number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Age Category ⭐ (Easy)    # 
+giving_age = int(input("Enter your age: "))
+if (giving_age < 13):
+    print("You are a child.")
+
+elif (giving_age >= 13 and giving_age <= 19):
+    print("You are a teenager.")
+
+elif(giving_age >= 20 and giving_age <= 59):
+    print("You are an adult.")
+elif(giving_age >= 60):
+    print("You are a senior citizen.")   
+
+
+    # 
+#  check the positive and nagative number 
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
